4496812eca370b4895624f1cfd94da2bd067d4c1
Florent Viel <fviel@wanadev.fr>
2013-07-08 17:27:50 +0200

    Add: extends Base Assplication

--- lib/DependenSees/Application.php
+++ lib/DependenSees/Application.php
@@ -3,12 +3,11 @@
 namespace DependenSees;
 
 use Symfony\Component\Console\Application as BaseApplication;
-use Symfony\Component\Console\Input\InputInterface;
 
 /**
  * @author Florent Viel <luxifer666@gmail.com>
  */
 class Application extends BaseApplication
 {
-    
+
 }
