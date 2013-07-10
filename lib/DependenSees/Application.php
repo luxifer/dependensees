701e7f4dec2a0091ca9d6d9db30153e8829f0331
Florent Viel <fviel@wanadev.fr>
2013-07-10 14:38:22 +0200

    Remove embedded composer

--- lib/DependenSees/Application.php
+++ lib/DependenSees/Application.php
@@ -3,20 +3,23 @@
 namespace DependenSees;
 
 use Symfony\Component\Console\Application as BaseApplication;
-use Dflydev\EmbeddedComposer\Core\EmbeddedComposerInterface;
 use DependenSees\Command\CheckCommand;
 use Symfony\Component\Console\Input\InputInterface;
+use Composer\IO\ConsoleIO;
+use Composer\Factory;
+use Composer\Json\JsonValidationException;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @author Florent Viel <luxifer666@gmail.com>
  */
 class Application extends BaseApplication
 {
-    protected $embeddedComposer;
+    protected $io;
+    protected $composer;
 
-    public function __construct(EmbeddedComposerInterface $embeddedComposer)
+    public function __construct()
     {
-        $this->embeddedComposer = $embeddedComposer;
         parent::__construct('DependenSees', '1.0.0-dev');
     }
 
@@ -26,13 +29,20 @@ class Application extends BaseApplication
         return 'dependensees';
     }
 
+    public function doRun(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new ConsoleIO($input, $output, $this->getHelperSet());
+
+        return parent::doRun($input, $output);
+    }
+
     protected function getDefaultCommands()
     {
         // Conserve les commandes par défaut du noyau pour avoir la
         // commande HelpCommand en utilisant l'option --help
         $defaultCommands = parent::getDefaultCommands();
 
-        $defaultCommands[] = new CheckCommand($this->embeddedComposer);
+        $defaultCommands[] = new CheckCommand();
 
         return $defaultCommands;
     }
@@ -44,5 +54,31 @@ class Application extends BaseApplication
         $inputDefinition->setArguments();
 
         return $inputDefinition;
+    }
+
+    /**
+     * @param  bool                    $required
+     * @throws JsonValidationException
+     * @return \Composer\Composer
+     */
+    public function getComposer($required = true)
+    {
+        if (null === $this->composer) {
+            try {
+                $this->composer = Factory::create($this->io);
+            } catch (\InvalidArgumentException $e) {
+                if ($required) {
+                    $this->io->write($e->getMessage());
+                    exit(1);
+                }
+            } catch (JsonValidationException $e) {
+                $errors = ' - ' . implode(PHP_EOL . ' - ', $e->getErrors());
+                $message = $e->getMessage() . ':' . PHP_EOL . $errors;
+                throw new JsonValidationException($message);
+            }
+
+        }
+
+        return $this->composer;
     }
 }
--- lib/DependenSees/Command/CheckCommand.php
+++ lib/DependenSees/Command/CheckCommand.php
@@ -2,28 +2,18 @@
 
 namespace DependenSees\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Dflydev\EmbeddedComposer\Core\EmbeddedComposerInterface;
-use Composer\IO\ConsoleIO;
 use Composer\Package\Version\VersionParser;
+use Composer\Repository\ArrayRepository;
 
 /**
  * @author Florent Viel <luxifer666@gmail.com>
  */
 class CheckCommand extends Command
 {
-    protected $embeddedComposer;
-
-    public function __construct(EmbeddedComposerInterface $embeddedComposer)
-    {
-        $this->embeddedComposer = $embeddedComposer;
-        parent::__construct('dependensees');
-    }
-
     protected function configure()
     {
         $this
@@ -34,22 +24,11 @@ class CheckCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $io = new ConsoleIO($input, $output, $this->getHelperSet());
-        $composer = $this->embeddedComposer->createComposer($io);
-        $rootPackage = $composer->getPackage();
-        $config = $composer->getConfig();
-        $requires = $rootPackage->getRequires();
-        $stability = $rootPackage->getMinimumStability();
-        $parser = new VersionParser();
-        $manager = $composer->getRepositoryManager();
-        $local = $manager->getLocalRepository();
-        $tmp = $manager->getRepositories();
-        $packagist = $tmp[0];
+        $package = $this->getComposer(false)->getPackage();
+        $requires = $package->getRequires();
 
         foreach ($requires as $name => $link) {
-            $match = $this->embeddedComposer->findPackage($name);
-            $string = sprintf('%s %s', str_pad($name, 30), $match->getPrettyVersion());
-            $output->writeLn($string);
+            $output->writeLn(sprintf('%s <comment>%s</comment>', str_pad($name, 30), $link->getPrettyConstraint()));
         }
     }
 }
